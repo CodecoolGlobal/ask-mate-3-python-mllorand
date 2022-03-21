8a39324e403cf59b146ef94e3afdd7350e92248a
Daniel Fekete <danielt.fekete@gmail.com>
2022-03-21 14:11:27 +0100

    Add starter objects
    util.py created for data handling
    index.html created for start planning

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import util
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+    questions = util.get_all_question()
+    header = util.QUESTION_HEADER
+    return render_template("index.html", header=header, questions=questions)
 
 
 if __name__ == "__main__":
