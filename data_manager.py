81e2caa33273014d6f8f723600ea5c7e90a64386
Daniel Fekete <danielt.fekete@gmail.com>
2022-03-21 14:59:05 +0100

    first task in progress

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,10 @@
 import os
+import connection
 
 QUESTION_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
 QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+
+
+def get_all_questions():
+    questions = connection.read_csv(QUESTION_FILE_PATH, QUESTION_HEADER)
+    return questions[::-1]
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template
-import util
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/list")
 def main():
-    questions = util.get_all_question()
-    header = util.QUESTION_HEADER
+    questions = data_manager.get_all_questions()
+    header = data_manager.QUESTION_HEADER
     return render_template("index.html", header=header, questions=questions)
 
 
