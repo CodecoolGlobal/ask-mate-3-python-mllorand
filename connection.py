feaf23f54f2560a1a78c99c5c66fb82eb35ed4c2
Daniel Fekete <danielt.fekete@gmail.com>
2022-03-22 09:30:38 +0100

    Update static content handle

--- connection.py
+++ connection.py
@@ -1,44 +1,13 @@
-import os
-import psycopg2
-import psycopg2.extras
+import csv
 
 
-def get_connection_string():
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
-    env_variables_defined = user_name and password and host and database_name
-    if env_variables_defined:
-        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            database_name=database_name
-        )
-    else:
-        raise KeyError('Some necessary environment variable(s) are not defined')
-
-
-def open_database():
+def read_csv(filename, header):
     try:
-        connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
-        connection.autocommit = True
-    except psycopg2.DatabaseError as exception:
-        print('Database connection problem')
-        raise exception
-    return connection
-
-
-def connection_handler(function):
-    def wrapper(*args, **kwargs):
-        connection = open_database()
-        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        ret_value = function(dict_cur, *args, **kwargs)
-        dict_cur.close()
-        connection.close()
-        return ret_value
-    return wrapper
-
-
+        with open(filename) as file:
+            questions = [question for question in csv.DictReader(file)]
+        return questions
+    except FileNotFoundError:
+        with open(filename, "w") as file:
+            file.write(",".join(header))
+            file.write("\n")
+        return read_csv(filename, header)
--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, render_template
-import util
+import data_manager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="/")
 
 
 @app.route("/")
+@app.route("/list")
 def main():
-    questions = util.get_all_question()
-    header = util.QUESTION_HEADER
+    if request.args:
+        print("must be sorted by "+request.args["sort_by"])
+    questions = data_manager.get_all_questions()
+    header = data_manager.QUESTION_HEADER
     return render_template("index.html", header=header, questions=questions)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
