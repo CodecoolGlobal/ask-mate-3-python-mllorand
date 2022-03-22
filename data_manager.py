b06286524aea7b8a4e40548b0b81696b6b8d1c83
Daniel Fekete <danielt.fekete@gmail.com>
2022-03-22 09:30:38 +0100

    Update static content handle

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,6 @@
 import os
 import connection
+from datetime import datetime
 
 QUESTION_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
 QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
@@ -7,4 +8,7 @@ QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title
 
 def get_all_questions():
     questions = connection.read_csv(QUESTION_FILE_PATH, QUESTION_HEADER)
-    return questions[::-1]
+    questions.reverse()
+    for question in questions:
+        question["submission_time"] = datetime.fromtimestamp(int(question["submission_time"])).strftime("%Y-%m-%d, %H:%M:%S")
+    return questions
