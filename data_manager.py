2126d5a268ec1e680475e59d4015b4490879e0e3
mllorand <m.lorandlevente@gmail.com>
2022-03-22 14:04:19 +0100

    implement delete answer feature

--- data_manager.py
+++ data_manager.py
@@ -1,18 +1,37 @@
+import csv
 import os
 import connection
 from datetime import datetime
 
 QUESTION_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
+ANSWER_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'answer.csv'
 QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
-def get_all_questions():
-    questions = connection.read_csv(QUESTION_FILE_PATH, QUESTION_HEADER)
-    questions.reverse()
-    for question in questions:
-        question["submission_time"] = datetime.fromtimestamp(int(question["submission_time"])).strftime("%Y-%m-%d, %H:%M:%S")
-    return questions
+def get_all_entries(file_path, file_header):
+    entries = connection.read_csv(file_path, file_header)
+    entries.reverse()
+    for entry in entries:
+        entry["submission_time"] = datetime.fromtimestamp(int(entry["submission_time"])).strftime("%Y-%m-%d, %H:%M:%S")
+    return entries
 
 
-def read_csv():
-    return connection.read_csv(QUESTION_FILE_PATH, QUESTION_HEADER)
+def get_entry_by_id(entry_id, file_path, file_header):
+    entries = connection.read_csv(file_path, file_header)
+    key = 'id'
+    value = entry_id
+    entry = next((entry for entry in entries if entry.get(key) == value), None)
+    return entry
+
+
+def delete_entry(entry_id, file_path, file_header):
+    entries = get_all_entries(file_path, file_header)
+    entry_to_delete = get_entry_by_id(entry_id, file_path, file_header)
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=file_header)
+        writer.writeheader()
+        for entry in entries:
+            if entry == entry_to_delete:
+                continue
+            writer.writerow(entry)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import data_manager
 
 app = Flask(__name__, static_folder="/")
@@ -25,6 +25,15 @@ def route_question(question_id):
                 dict_for_show = line
     header = data_manager.QUESTION_HEADER
     return render_template("question.html", header=header, dict_for_show=dict_for_show)
+
+
+@app.route("/answer/<answer_id>/delete")
+def route_answer(answer_id=None):
+    if answer_id:
+        answer = data_manager.get_entry_by_id(answer_id, data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER)
+        question_id = answer['question_id']
+        data_manager.delete_entry(answer_id, data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER)
+        return redirect("/question/" + question_id)
 
 
 if __name__ == "__main__":
