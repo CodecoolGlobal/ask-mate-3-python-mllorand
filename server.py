b0c086a540fbb0a5fc0a43ee3c4d6a171876c632
mllorand <m.lorandlevente@gmail.com>
2022-03-22 13:32:14 +0100

    generalize data_manager functions

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import data_manager
 
 app = Flask(__name__, static_folder="/")
@@ -12,6 +12,14 @@ def main():
     questions = data_manager.get_all_questions()
     header = data_manager.QUESTION_HEADER
     return render_template("index.html", header=header, questions=questions)
+
+
+@app.route("/answer/<answer_id>/delete")
+def route_answer(answer_id=None):
+    if answer_id:
+        return redirect(("question/"))
+
+
 
 
 if __name__ == "__main__":
