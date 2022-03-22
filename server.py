a89f0e644d2b3997139ef27f1d7e2ead90b7df0e
Daniel Fekete <danielt.fekete@gmail.com>
2022-03-22 09:30:38 +0100

    Update static content handle

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, render_template
 import data_manager
 
-app = Flask(__name__, static_folder="/home/fekete/codecool/web_projects/ask-mate-1-python-horvatht9411/images")
+app = Flask(__name__, static_folder="/")
 
 
 @app.route("/")
 @app.route("/list")
 def main():
+    if request.args:
+        print("must be sorted by "+request.args["sort_by"])
     questions = data_manager.get_all_questions()
     header = data_manager.QUESTION_HEADER
     return render_template("index.html", header=header, questions=questions)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
