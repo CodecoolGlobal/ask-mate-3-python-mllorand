13c049a4f7a8ad2c78459fc9ff7220d5c59205dd
danielt.fekete <danielt.fekete@gmail.com>
2022-03-24 15:37:19 +0100

    add utf-8 encodin to read/write csv

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ import csv
 
 def read_csv(file_path, header):
     try:
-        with open(file_path) as file:
+        with open(file_path, encoding="utf-8") as file:
             entries = [entry for entry in csv.DictReader(file)]
         return entries
     except FileNotFoundError:
@@ -14,7 +14,7 @@ def read_csv(file_path, header):
 
 
 def write_csv(file_path, header, entries):
-    with open(file_path, 'w', newline='') as file:
+    with open(file_path, 'w', newline='', encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
         for entry in entries:
--- server.py
+++ server.py
@@ -37,11 +37,23 @@ def route_question(question_id):
                                        question=question, question_id=question_id, answers=answers)
 
 
+@app.route("/question/<question_id>/delete")
+def route_delete_question(question_id):
+    question = data_manager.get_entry_by_id(question_id, data_manager.QUESTION_FILE_PATH, data_manager.QUESTION_HEADER)
+    data_manager.delete_entry(data_manager.QUESTION_FILE_PATH, data_manager.QUESTION_HEADER, question)
+    answers_to_remove = [answer for answer in
+                         data_manager.get_all_entries_with_unix_timestamp(data_manager.ANSWER_FILE_PATH,
+                                                                          data_manager.ANSWER_HEADER)
+                         if answer['question_id'] == question_id]
+    for answer in answers_to_remove:
+        data_manager.delete_entry(data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER, answer)
+    return redirect("/list")
+
+
 @app.route("/question/<question_id>/new-answer", methods=['GET', 'POST'])
 def route_add_answer(question_id):
     if request.method == 'GET':
         return render_template("answer.html", question_id=question_id)
-    new_answer = request.form
     if request.files.get('image').content_type == 'application/octet-stream':
         path = './uploaded_files/no_image_found.png'
     else:
@@ -49,12 +61,12 @@ def route_add_answer(question_id):
         path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
         image.save(path)
     data_manager.add_new_entry(data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER,
-                               entry_to_add=new_answer, upload_path=path)
+                               entry_to_add=request.form, upload_path=path)
     return redirect("/question/" + question_id)
 
 
 @app.route("/answer/<answer_id>/delete")
-def route_answer(answer_id):
+def route_delete_answer(answer_id):
     answer = data_manager.get_entry_by_id(answer_id, data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER)
     question_id = answer['question_id']
     data_manager.delete_entry(data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER, answer)
@@ -64,7 +76,6 @@ def route_answer(answer_id):
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_add_question():
     if request.method == 'POST':
-        new_question = request.form
         if request.files.get('image').content_type == 'application/octet-stream':
             path = './uploaded_files/no_image_found.png'
         else:
@@ -72,9 +83,21 @@ def route_add_question():
             path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(path)
         new_question = data_manager.add_new_entry(data_manager.QUESTION_FILE_PATH, data_manager.QUESTION_HEADER,
-                                                  entry_to_add=new_question, upload_path=path)
+                                                  entry_to_add=request.form, upload_path=path)
         return redirect('/question/'+str(new_question['id']))
     return render_template('add_question.html', question=data_manager.QUESTION_HEADER)
+
+
+@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
+def route_edit_question(question_id):
+    question = data_manager.get_entry_by_id(question_id, data_manager.QUESTION_FILE_PATH,
+                                            data_manager.QUESTION_HEADER)
+    if request.method == 'GET':
+        return render_template('edit_question.html', question_headers=data_manager.QUESTION_HEADER, question=question)
+    if request.method == 'POST':
+        data_manager.update_entry(data_manager.QUESTION_FILE_PATH, data_manager.QUESTION_HEADER,
+                                  entry_to_update=request.form)
+        return redirect('/question/' + question_id)
 
 
 @app.route("/answer/<answer_id>/<vote>", methods=["GET"])
