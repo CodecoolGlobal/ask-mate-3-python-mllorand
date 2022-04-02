e4d47333f59a19f1919169436e6a5cabcbb1900f
appuser <appuser@journey2-7d688774f7-l49gl>
2022-04-02 14:16:58 +0000

    starter project

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -1,5 +1,3 @@
--- Custom configurations:
-set time zone 'Europe/Budapest';
 --
 -- PostgreSQL database dump
 --
@@ -21,22 +19,22 @@ ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_tag_
 DROP TABLE IF EXISTS public.question;
 CREATE TABLE question (
     id serial NOT NULL,
-    submission_time timestamp without time zone default now()::timestamp(0),
-    view_number integer default 0,
-    vote_number integer default 0,
+    submission_time timestamp without time zone,
+    view_number integer,
+    vote_number integer,
     title text,
     message text,
-    image text default 'no_image_found.png'
+    image text
 );
 
 DROP TABLE IF EXISTS public.answer;
 CREATE TABLE answer (
     id serial NOT NULL,
-    submission_time timestamp without time zone default now()::timestamp(0),
-    vote_number integer default 0,
-    question_id integer default 0,
+    submission_time timestamp without time zone,
+    vote_number integer,
+    question_id integer,
     message text,
-    image text default 'no_image_found.png'
+    image text
 );
 
 DROP TABLE IF EXISTS public.comment;
@@ -45,8 +43,8 @@ CREATE TABLE comment (
     question_id integer,
     answer_id integer,
     message text,
-    submission_time timestamp without time zone default now()::timestamp(0),
-    edited_count integer default 0
+    submission_time timestamp without time zone,
+    edited_count integer
 );
 
 
@@ -79,21 +77,21 @@ ALTER TABLE ONLY tag
     ADD CONSTRAINT pk_tag_id PRIMARY KEY (id);
 
 ALTER TABLE ONLY comment
-    ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id) on delete cascade;
+    ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id);
 
 ALTER TABLE ONLY answer
-    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id) on delete cascade;
+    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
 
 ALTER TABLE ONLY question_tag
-    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id) on delete cascade;
+    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
 
 ALTER TABLE ONLY comment
-    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id) on delete cascade;
+    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
 
 ALTER TABLE ONLY question_tag
-    ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag(id) on delete cascade;
+    ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag(id);
 
-INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', 'no_image_found.png');
+INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
 INSERT INTO question VALUES (1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
 
 I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine.
@@ -102,13 +100,13 @@ BUT in my theme i also using jquery via webpack so the loading order is now foll
 
 jquery
 booklet
-app.js (bundled file with webpack, including jquery)', 'no_image_found.png');
+app.js (bundled file with webpack, including jquery)', 'images/image1.png');
 INSERT INTO question VALUES (2, '2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
-', 'no_image_found.png');
+', NULL);
 SELECT pg_catalog.setval('question_id_seq', 2, true);
 
-INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', 'no_image_found.png');
-INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'no_image_found.png');
+INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
+INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
 SELECT pg_catalog.setval('answer_id_seq', 2, true);
 
 INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
--- server.py
+++ server.py
@@ -1,76 +1,12 @@
-from flask import Flask, render_template
-from bonus_questions import SAMPLE_QUESTIONS
+from flask import Flask
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './static/images'
-
-
-@app.route("/static/images/<path:filename>")
-def images(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-@app.route("/catch-hacker")
-def catch_hacker():
-    return render_template('hack.html')
 
 
 @app.route("/")
-def load_main():
-    return render_template('index.html', payload=data_manager.get_main_page_data(request.args))
-
-
-@app.route("/list")
-def load_list_page():
-    if request.args.get('order'):
-        if request.args.get('order').lower() not in ['asc', 'desc']:
-            return redirect(url_for('catch_hacker'))
-    return render_template("index.html", payload=data_manager.get_list_page_data(request.args))
-
-
-@app.route("/question/<question_id>")
-def load_question_page(question_id):
-    return render_template("question.html", payload=data_manager.get_question_page_data(question_id, request.args))
-
-
-@app.route("/<table>/<record_id>/delete")
-@app.route("/question/<question_id>/<table>/<record_id>/delete")
-def delete_record_by_id(table, record_id, question_id=None):
-    if table != 'question_tag':
-        data_manager.delete_record_by_identifier(table, record_id, question_id, 'id')
-    else:
-        data_manager.delete_record_by_identifier(table, record_id, question_id, 'tag_id')
-    if table == 'question':
-        return redirect(url_for('load_list_page'))
-    return redirect(request.referrer)
-
-
-@app.route("/vote_on_record", methods=['POST'])
-def vote_on_record():
-    table = request.form["table"]
-    data_manager.update_record(table, request.form)
-    return '', 202
-
-
-@app.route("/question/<question_id>/new-<record>/", methods=['GET', 'POST'])
-@app.route("/answer/<answer_id>/new-<record>", methods=['GET', 'POST'])
-@app.route("/add-<record>", methods=['GET', 'POST'])
-def add_new_record(record, question_id=None, answer_id=None):
-    if request.method == 'POST':
-        data_manager.add_new_record(record, question_id, answer_id, request)
-        if record != 'question' and request.form.get('redirect'):
-            return redirect(request.form['redirect'])
-        question_id = data_manager.get_fields_from_table_by_value('id', 'question', ordered=True)['id']
-        return redirect(url_for('load_question_page', question_id=question_id))
-    return render_template('add_new_record.html', payload={'record': record,
-                                                           'question_id': question_id,
-                                                           'answer_id': answer_id,
-                                                           'redirect': request.referrer})
-
-@app.route("/bonus-questions")
-def main():
-    return render_template('bonus_questions.html', questions=SAMPLE_QUESTIONS)
+def hello():
+    return "Hello World!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
