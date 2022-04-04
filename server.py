020352d2ae103ac30422dc9a676e823bfa4cc785
Daniel Fekete <danielt.fekete@gmail.com>
2022-04-04 13:12:43 +0200

    merge askmate1

--- server.py
+++ server.py
@@ -1,18 +1,12 @@
-from flask import Flask, request, render_template
-import data_manager
+from flask import Flask
 
-app = Flask(__name__, static_folder="/")
+app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/list")
-def main():
-    if request.args:
-        print("must be sorted by "+request.args["sort_by"])
-    questions = data_manager.get_all_questions()
-    header = data_manager.QUESTION_HEADER
-    return render_template("index.html", header=header, questions=questions)
+def hello():
+    return "Hello World!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
