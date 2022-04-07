d3fcf78fecde4d254d5120055c6cf5541dfc790c
mllorand <m.lorandlevente@gmail.com>
2022-04-07 13:52:53 +0200

    implement add comment to answer to DB feature

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def main():
                                        order='desc',
                                        limit='5')
     return render_template("index.html",
-                           questions=questions,
+                           cards=questions,
                            columns=column_names)
 
 
@@ -31,7 +31,21 @@ def list():
                                            columns=column_names,
                                            sort_by=request.args['sort_by'],
                                            order=request.args['order'])
-    return render_template("index.html", questions=questions, columns=column_names)
+    return render_template("index.html", cards=questions, columns=column_names)
+
+
+@app.route("/search")
+def search():
+    column_names = ["vote_number", "a_vote_number", "submission_time", "a_submission_time"]
+    sort_by = request.args['sort_by'] if request.args.get('sort_by') in column_names else None
+    order = request.args['order'] if sort_by else None
+    if len(request.args['q'].strip()) > 0:
+        search_result = data_manager.get_records_by_search(word=request.args['q'],
+                                                           sort_by=sort_by,
+                                                           order=order)
+    else:
+        return redirect(url_for('main'))
+    return render_template("search.html", cards=search_result, columns=column_names)
 
 
 @app.route("/question/<question_id>")
@@ -44,11 +58,12 @@ def route_question(question_id):
                            tags=data_manager.tag_table(question_id),
                            answers=data_manager.get_table('answer', selector='question_id',
                                                           selected_value=question_id),
-                           # comment_for_answers=data_manager.get_table(''),
+                           comment_for_answers=data_manager.get_table('comment'),
                            answer_headers=data_manager.get_column_names('answer'),
                            question_id=question_id)
 
 
+@app.route("/")
 @app.route("/question/<question_id>/delete")
 def route_delete_question(question_id):
     data_manager.delete_record_by_id('question', 'id', question_id)
@@ -66,14 +81,14 @@ def route_delete_question(question_id):
 def route_add_answer(question_id):
     if request.method == 'GET':
         return render_template("answer.html", question_id=question_id)
-    if request.files.get('image').content_type == 'application/octet-stream':
-        path = './uploaded_files/no_image_found.png'
+    if not request.files.get('image'):
+        path = './static/images/no_image_found.png'
     else:
         image = request.files['image']
         path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
         image.save(path)
     form = dict(request.form)
-    form['submission_time'] = datetime.datetime.now()
+    form['submission_time'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     form['image'] = path
     data_manager.add_new_record('answer', form)
     return redirect("/question/" + question_id)
@@ -92,14 +107,16 @@ def route_delete_answer(answer_id):
 def route_add_question():
     if request.method == 'POST':
         if request.files.get('image').content_type == 'application/octet-stream':
-            path = './uploaded_files/no_image_found.png'
+            path = 'images/no_image_found.png'
         else:
             image = request.files['image']
             path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(path)
         form = dict(request.form)
-        form['submission_time'] = datetime.datetime.now()
+        form['submission_time'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         form['image'] = path
+        form['vote_number'] = form.get('vote_number', 0)
+        form['view_number'] = form.get('view_number', 0)
         data_manager.add_new_record('question', form)
         question_id = str(data_manager.get_table('question', columns=['id'],
                                                  sort_by='id', order='desc')[0].get('id'))
@@ -114,6 +131,8 @@ def route_add_tag(question_id):
         tag_id = data_manager.get_table('tag', columns=['id'])[-1:]
         data_manager.tag_to_question_tag(question_id, tag_id)
         return redirect(url_for('route_add_tag', question_id=question_id))
+    if request.method == 'GET':
+        data_manager.add_existing_tag_to_question_tag(question_id, request.args.values())
     tags = data_manager.get_table(table='tag')
     return render_template('add_tags.html', question_id=question_id, tags=tags)
 
@@ -133,16 +152,12 @@ def route_edit_question(question_id):
 @app.route("/question/<question_id>/<vote>'", methods=["GET", "POST"])
 def add_vote(vote, answer_id=None, question_id=None):
     if question_id:
-        data_manager.vote_on_entry(data_manager.QUESTION_FILE_PATH, data_manager.QUESTION_HEADER,
-                                   vote=vote, entry_id=question_id)
-        return redirect(request.form["original_url"])
+        data_manager.update_vote_number('question', question_id, vote)
+        return redirect('question/' + question_id)
     elif answer_id:
-        data_manager.vote_on_entry(data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER,
-                                   vote=vote, entry_id=answer_id)
-        answer = data_manager.get_entry_by_id(answer_id,
-                                              data_manager.ANSWER_FILE_PATH, data_manager.ANSWER_HEADER)
-        print("vote on answer in progress")
-        return redirect("/question/"+answer["question_id"])
+        data_manager.update_vote_number('answer', answer_id, vote)
+        answer = data_manager.get_table('answer', selector='id', selected_value=answer_id)[0]
+        return redirect('/question/' + str(answer.get('question_id')))
 
 
 @app.route('/question/<question_id>/new-comment', methods=['GET', 'POST'])
@@ -155,10 +170,13 @@ def route_add_comment_to_question(question_id):
     return render_template('add_comment.html', question_id=question_id)
 
 
-@app.route('/question/<question_id>/new-comment', methods=['GET', 'POST'])
-def route_add_comment_to_answer(question_id, answer_id):
+@app.route('/answer/<answer_id>/new-comment', methods=['GET', 'POST'])
+def route_add_comment_to_answer(answer_id):
+    question_id = data_manager.get_table('answer', selector='id', selected_value=answer_id)[0].get('question_id')
     if request.method == "POST":
-        data_manager.add_new_record('comment', request.form)
+        form = dict(request.form)
+        form['submission_time'] = datetime.datetime.now()
+        data_manager.add_new_record('comment', form)
         return redirect(url_for('route_question', question_id=question_id))
     return render_template('add_comment_to_answer.html', answer_id=answer_id)
 
@@ -181,6 +199,14 @@ def route_edit_answer(answer_id):
     data_manager.update_message('answer', answer_id, request.form.get('message'))
     return redirect('/question/' + str(answer.get('question_id')))
 
+
+@app.route("/comments/<comment_id>/delete", methods=['POST', 'GET', 'DELETE'])
+def delete_comment(comment_id):
+    print(comment_id)
+    question_id = data_manager.get_table('comment', columns=['question_id'], selector='id', selected_value=comment_id)
+    data_manager.delete_record_by_id('comment', selector='id', selected_value=comment_id)
+    for cell in question_id:
+        return redirect(url_for("route_question", question_id=cell.get('question_id')))
 
 
 
