906c21e09c87eef2d2f88ce398f97f2f3b7257f0
horvatht9411 <horvatht9411@gmail.com>
2022-04-20 14:05:20 +0200

    feat: bind question to user

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -1,3 +1,8 @@
+alter table question
+    add user_id int;
+
+
+
 -- Custom configurations:
 set time zone 'Europe/Budapest';
 --
@@ -26,7 +31,8 @@ CREATE TABLE question (
     vote_number integer default 0,
     title text,
     message text,
-    image text default 'no_image_found.png'
+    image text default 'no_image_found.png',
+    owner text
 );
 
 DROP TABLE IF EXISTS public.answer;
