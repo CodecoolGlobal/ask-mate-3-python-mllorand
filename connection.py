0ef7b8d82a3ffa74615e542488601f2c35d898b0
danielt.fekete <danielt.fekete@gmail.com>
2022-05-24 15:55:57 +0200

    init deploy

--- connection.py
+++ connection.py
@@ -22,7 +22,7 @@ def get_connection_string():
 
 def open_database():
     try:
-        connection_string = get_connection_string()
+        connection_string = os.environ.get('DATABASE_URL')
         connection = psycopg2.connect(connection_string)
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
